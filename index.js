4f454ea84a881658631af5881186368074e55e9f
Noel Miranda <noelmrnd@gmail.com>
2022-05-25 11:01:58 -0600

    Corrección en exportación de clases.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import Printer from "./src/Printer";
 import PrinterList from "./src/Utils/PrinterList";
 import PrintTemplate from "./src/Utils/PrintTemplate";
 
-exports.Printer = Printer;
-exports.PrinterList = PrinterList;
-exports.PrintTemplate = PrintTemplate;
+module.exports = {
+    Printer,
+    PrinterList,
+    PrintTemplate
+};
